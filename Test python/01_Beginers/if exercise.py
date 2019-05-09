d4471e89eb8cae44858330f28f15816cc9a61ff8
gblagoev <gblagoev@htmlbox.net>
2019-05-09 17:58:11 +0300

    add if_exerxise

---
+++ Test python/01_Beginers/if exercise.py
@@ -0,0 +1,11 @@
+x = True
+y = False
+
+if x or y:
+    print("Това е примерно за 'OR' ")
+    print("Едно от двете е вярно 'True'")
+
+else:
+    print("Това е примерно за 'OR' ")
+    print("И двете са грешни'False'")
+
